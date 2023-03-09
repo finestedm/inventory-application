0dc77ef64e8faf252ba5edeb3acbb2b61158a2b8
Paweł <finestedm@gmail.com>
2023-03-09 17:47:58 +0100

    iteration of object catalogCount working

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -3,24 +3,29 @@ import { useEffect, useState } from "react"
 
 export default function Catalog() {
 
+    // declare types in catalogCounts
+    interface ICatalogCounts {
+        [key: string]: number;
+     }
 
-
-    const [catalogCounts, setCatalogCount] = useState({
-        partCount: 0
+    // set initial states of catalogCounts with declared types of data
+    const [catalogCounts, setCatalogCount] = useState<ICatalogCounts>({
+        partCount: 0,
+        somethingElse: 3
     })
 
+    // get data from server: number of elements in each category
     useEffect(() => {
         axios.get(`${window.location.origin}${window.location.pathname}`)
             .then((response) => setCatalogCount(response.data))
     }, [])
 
-    
 
     return (
         <div>
             <h2>Summary of the catalog</h2>
             <ul>
-                {(Object.entries(catalogCounts)).map(object => <li>{catalogCounts[object]}</li>)}
+                {Object.keys(catalogCounts).map(key => <li>{key}: {catalogCounts[key]}</li>)}
             </ul>
         </div>
     )
