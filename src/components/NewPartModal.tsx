239ae6e3f7d10a5c7957a85471f24110f1a851db
Paweł <finestedm@gmail.com>
2023-03-18 13:21:05 +0100

    added textfield to newPartModal - not working yet

--- src/components/NewPartModal.tsx
+++ src/components/NewPartModal.tsx
@@ -1,9 +1,24 @@
-import { Modal , Box, Typography, Card, CardContent, CardHeader, TextField, FormControl, InputAdornment} from "@mui/material";
+import { Modal , Box, Typography, Card, CardContent, CardHeader, TextField, FormControl, InputAdornment, Autocomplete} from "@mui/material";
+import { useState } from "react";
 
 
 export default function NewPartModal(props: { newPartModalOpen: boolean, setNewPartModalOpen: React.Dispatch<React.SetStateAction<boolean>> }) {
     const { newPartModalOpen, setNewPartModalOpen } = props
-    console.log(props)
+
+    interface INewPartData {
+        name: string;
+        price: number;
+        manufacturer: string;
+        tags: string[]
+    }
+
+    const [newPartData, setNewPartData] = useState<INewPartData>({
+        name: '',
+        price: 0,
+        manufacturer: '',
+        tags: []
+    })
+
     return (
         <Modal open={newPartModalOpen} onClose={() => setNewPartModalOpen(false)}>
             <Card sx={{ position: 'absolute', top: '50%', left: '50%', transform: "translate(-50%, -50%)" }}>
@@ -12,11 +27,24 @@ export default function NewPartModal(props: { newPartModalOpen: boolean, setNewP
                 <FormControl>
                     <TextField
                         label='New part name'
+                        value={newPartData.name}
                     />
                     <TextField
                         label='Price'
+                        value={newPartData.price}    
                         InputProps={{endAdornment: <InputAdornment disableTypography position="end">PLN</InputAdornment>}}
-                    />     
+                    />
+                    <Autocomplete
+                        options={['a', 'b', 'c']}
+                        renderInput={(p) => <TextField {...p} label="Tags" />}
+                        freeSolo
+                        multiple
+                    />
+                    <Autocomplete
+                        options={['a', 'b', 'c']}
+                        renderInput={(p) => <TextField {...p} label="Manufacturer" />}
+                        freeSolo
+                    />
                 </FormControl>
                 </CardContent>
             </Card>
