a324649a12fad7b52410cd9d1677c4553f720c21
Paweł <finestedm@gmail.com>
2023-04-01 10:05:20 +0200

    responsive navbar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,16 +1,91 @@
-import { AppBar, Button, Container, Toolbar, Typography } from '@mui/material';
+import { AppBar, Avatar, Box, Button, Container, IconButton, Toolbar, Tooltip, Typography, Menu, MenuItem } from '@mui/material';
+import AdbIcon from '@mui/icons-material/Adb';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from 'react';
+import axios from 'axios';
 
 export default function TopBar() {
+
+    // responsive navbar logic
+    const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+    const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    const pages = [
+        { name: 'Parts', path: '/parts' },
+        { name: 'Shops', path: '/locations' },
+        { name: 'Tags', path: '/tags' }
+    ];
 
     return (
         <AppBar position="static" sx={{ backgroundColor: 'white', boxShadow: 'none' }}>
             <Container maxWidth='xl' sx={{ flexGrow: 1 }} >
-                <Toolbar>
-                    <Button href='/'>
-                        <Typography variant="h5" component="div" color='text.primary' sx={{ flexGrow: 1, fontWeight: 700 }}>
-                            Untitled store
-                        </Typography>
-                    </Button>
+                <Toolbar disableGutters sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Typography href='/' variant="h5" component="a" color='text.primary' sx={{ fontWeight: 700 }}>
+                        Untitled store
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, justifyContent: 'flex-end' }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="primary"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page.name} onClick={handleCloseNavMenu}>
+                                    <Typography component='a' href={`${window.location.origin}/catalog${page.path}`} textAlign="center">{page.name}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'flex-end', }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page.name}
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, display: 'block' }}
+                                href={`${window.location.origin}/catalog${page.path}`}
+                            >
+                                {page.name}
+                            </Button>
+                        ))}
+                    </Box>
                 </Toolbar>
             </Container>
         </AppBar>
