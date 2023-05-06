75add37390a6d315ea51c0ce6456035413c5a2fa
Paweł <finestedm@gmail.com>
2023-05-06 13:45:36 +0200

    small footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Typography, Container, useTheme } from "@mui/material";
+import { Box, Stack, Typography, Container, useTheme, Grid } from "@mui/material";
 import { blueGrey } from "@mui/material/colors";
 
 export default function Footer(): JSX.Element {
@@ -6,25 +6,29 @@ export default function Footer(): JSX.Element {
     return (
         <Box component='footer' sx={{ backgroundColor: `${blueGrey[900]}`, color: 'white', pb: 2, pt: 4, mt: 'auto' }}>
             <Container maxWidth="xl">
-                <Stack className="footer--stack-top" direction='row' flex={1} justifyContent='space-between'>
-                    <img alt="Logo"></img>
-                    <Stack className="footer--stack-nav" direction='row'>
-                        <Stack className="footer--stack-nav-1">
-                            <Typography variant='h6'>Link A</Typography>
-                            <a href="">Link 1</a>
-                            <a href="">Link 2</a>
-                            <a href="">Link 3</a>
-                            <a href="">Link 4</a>
+                <Grid container className="footer--stack-top" direction='row' flex={1} justifyContent='space-between' spacing={2}>
+                    <Grid item xs={12} sm={6} md={8}>
+                        <img alt="Logo" />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <Stack className="footer--stack-nav" direction='row' justifyContent='space-between' spacing={5}>
+                            <Stack className="footer--stack-nav-1">
+                                <Typography variant='h6'>Link A</Typography>
+                                <a href="">Link 1</a>
+                                <a href="">Link 2</a>
+                                <a href="">Link 3</a>
+                                <a href="">Link 4</a>
+                            </Stack>
+                            <Stack className="footer--stack-nav-2">
+                                <Typography variant='h6'>Link B</Typography>
+                                <a href="">Link 1</a>
+                                <a href="">Link 2</a>
+                                <a href="">Link 3</a>
+                                <a href="">Link 4</a>
+                            </Stack>
                         </Stack>
-                        <Stack className="footer--stack-nav-2">
-                            <Typography variant='h6'>Link B</Typography>
-                            <a href="">Link 1</a>
-                            <a href="">Link 2</a>
-                            <a href="">Link 3</a>
-                            <a href="">Link 4</a>
-                        </Stack>
-                    </Stack>
-                </Stack>
+                    </Grid>
+                </Grid>
                 <Stack direction='row' flex={1}>
                     <p style={{ marginRight: 'auto' }}>all right reserved blablabla</p>
                     <a>Instagram Link</a>
