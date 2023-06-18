508d33cf5d5f5c9070ce6c6a34f9535334c8b3e9
Paweł <finestedm@gmail.com>
2023-06-18 13:57:01 +0200

    added uncontrolled search bar to top bar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Button, Container, Stack, SvgIcon, Toolbar, Typography } from '@mui/material';
+import { AppBar, Button, Container, FormControl, Stack, SvgIcon, TextField, Toolbar, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import logo from '../images/logo.svg'
 
@@ -8,7 +8,7 @@ export default function TopBar() {
 
     useEffect(() => {
         const handleScroll = () => {
-            const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            const scrollTop = window.scrollY || document.documentElement.scrollTop;
             setIsAtTop(scrollTop === 0);
         };
 
@@ -21,11 +21,17 @@ export default function TopBar() {
         <AppBar position="sticky" sx={{ height: isAtTop ? '5rem' : '3rem', transition: 'all .25s ease' }}>
             <Container maxWidth="xl" sx={{ flexGrow: 1, display: "flex" }}>
                 <Toolbar variant="dense" disableGutters sx={{ width: '100%' }}>
-                    <Stack direction='row' sx={{ flexGrow: 1, display: "flex", alignItems: "center" }}>
+                    <Stack direction='row' sx={{ flexGrow: 1, display: "flex", alignItems: "center" }} spacing={4}>
                         <Typography variant="h5" component="a" href="/" color="text.primary" sx={{ flexGrow: 1, fontWeight: 700, me: 'auto' }}>
-                            Untitled store
+                            Untitled
                         </Typography>
-
+                        <FormControl component={Stack}>
+                            <TextField
+                                sx={{minWidth: '3ch'}}
+                                size='small'
+                                placeholder='Search'
+                            />
+                        </FormControl>
                         <Stack alignItems="center">
                             <div style={{ backgroundImage: `url(${logo})`, width: '100%', minHeight: '32px', aspectRatio: '1/1' }} />
                             {isAtTop ? <Typography sx={{ visibility: isAtTop ? 'visible' : 'collapse', transition: 'all .25s ease' }}>Icon</Typography> : ''}
